130ed40a12e38fd8083b6950b5f50038ca4e7fb3
Minatallah Hisham <mennahishamh@gmail.com>
2023-11-02 12:51:13 +0200

    create handler

---
+++ route-app/pages/api/example.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const body = req.body;
+
+  res.status(200).end();
+};
+
+export default handler;
